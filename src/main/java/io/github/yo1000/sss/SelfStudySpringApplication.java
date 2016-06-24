33aaa2cbfb077bd6fb0bd1d7958d16ed4dd092ae
yo1000 <yoichi.kikuchi.aug@gmail.com>
2016-06-25 03:21:55 +0900

    Add chapter1 contents

---
+++ src/main/java/io/github/yo1000/sss/SelfStudySpringApplication.java
@@ -0,0 +1,12 @@
+package io.github.yo1000.sss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfStudySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfStudySpringApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/yo1000/sss/SelfStudySpringApplicationTests.java
@@ -0,0 +1,18 @@
+package io.github.yo1000.sss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SelfStudySpringApplication.class)
+@WebAppConfiguration
+public class SelfStudySpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
